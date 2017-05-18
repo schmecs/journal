640e720b2bb9c09e5fb6f86ea5ecf76d35028040
Rebecca <rblum@patientslikeme.com>
2017-05-18 08:57:17 -0400

    hashmap post structure

--- com/moodjournal/model/Journal.java
+++ com/moodjournal/model/Journal.java
@@ -11,3 +11,25 @@ public class Journal {
   public Journal() {
     mPosts = new ArrayList<Post>();
   }
+
+  public void addPost(Post post) {
+    mPosts.add(post);
+  }
+  
+  public int getPostCount() {
+    return mPosts.size();
+  }
+  
+  // FIXME:  This should be cached!
+  private Map<String, List<Post>> byAuthor() {
+    Map<String, List<Post>> byAuthor = new HashMap<>();
+    for (Post post : mPosts) {
+      List<Post> authorPosts = byAuthor.get(post.getAuthor());
+      if (authorPosts == null) {
+        authorPosts = new ArrayList<Post>();
+        byAuthor.put(post.getAuthor(), authorPosts);
+      }
+      authorPosts.add(post);
+    }
+    return byAuthor;
+  }
--- com/moodjournal/model/Post.java
+++ com/moodjournal/model/Post.java
@@ -8,36 +8,32 @@ import java.util.Map;
 
 public class Post {
 
-	private String mScore;
-	private String mText;
-	private Date mDate;
 	private Map mPostInfo;
 
-	public Post(int score, String text, Date date) {
-		mScore = Integer.toString(score);
-		mText = text;
-		mDate = date;
+	public Post(String author, int score, String text, Date date) {
+
 		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM-dd-yyyy");
-		String mDateFormat = DATE_FORMAT.format(mDate);
 		mPostInfo = new HashMap<String,String>();
-		mPostInfo.put("score",mScore);
-		mPostInfo.put("text",mText);
-		mPostInfo.put("date",mDateFormat);
+		mPostInfo.put("score",Integer.toString(score));
+		mPostInfo.put("text",text);
+		mPostInfo.put("date",DATE_FORMAT.format(date));
+		mPostInfo.put("author",author);
 	}
 
 	public String getScore() {
-		//return mScore;
 		return (String) mPostInfo.get("score");
 	}
 
 	public String getText() {
-		//return mText;
 		return (String) mPostInfo.get("text");
 	}
 
 	public String getDate() {
-		//return mDate;
 		return (String) mPostInfo.get("date");
+	}
+
+	public String getAuthor() {
+		return (String) mPostInfo.get("author");
 	}
 
 
