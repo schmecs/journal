e127afa0eb1e14fb0f796f4b8a2c53d8d830a770
Rebecca <rblum@patientslikeme.com>
2017-07-11 10:00:41 -0400

    placeholder text input on entry activity

--- app/src/main/java/com/schmecs/journal/EntryActivity.java
+++ app/src/main/java/com/schmecs/journal/EntryActivity.java
@@ -5,8 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import static com.schmecs.journal.R.menu.menu_main;
 
 public class EntryActivity extends AppCompatActivity {
 
@@ -17,17 +21,34 @@ public class EntryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
 
-        Button goHome = (Button) findViewById(R.id.bt_go_home);
-        goHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchHome();
-            }
-        });
     }
 
     public void launchHome() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.home:
+                this.launchHome();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
