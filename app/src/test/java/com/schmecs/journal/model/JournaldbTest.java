5f2a2614a321bd09280364ebed651e895912c474
Rebecca <rblum@patientslikeme.com>
2017-10-24 08:55:06 -0400

    future test file for db

---
+++ app/src/test/java/com/schmecs/journal/model/JournaldbTest.java
@@ -0,0 +1,28 @@
+package com.schmecs.journal.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JournaldbTest {
+    @Test
+    public void getNextUserPostId() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+    @Test
+    public void selectByAuthor() throws Exception {
+
+    }
+
+    @Test
+    public void nextId() throws Exception {
+
+    }
+
+}
